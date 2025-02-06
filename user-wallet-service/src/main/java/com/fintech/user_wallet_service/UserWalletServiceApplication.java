0c095bca285aaa6f5c63a987e45c91ac4997d1b4
Gourav Singh <gourav.oct@gmail.com>
2025-02-06 14:30:37 +0530

    user-wallet initial setup

---
+++ user-wallet-service/src/main/java/com/fintech/user_wallet_service/UserWalletServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fintech.user_wallet_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserWalletServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserWalletServiceApplication.class, args);
+	}
+
+}
---
+++ user-wallet-service/src/test/java/com/fintech/user_wallet_service/UserWalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.user_wallet_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserWalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
