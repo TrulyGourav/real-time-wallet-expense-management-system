bc333b7cab5a8efb56b3913686765bf33e86489f
Gourav Singh <gourav.oct@gmail.com>
2025-02-10 15:50:57 +0530

    modified Role enum

--- user-wallet-service/src/main/java/com/fintech/user_wallet_service/constant/Role.java
+++ user-wallet-service/src/main/java/com/fintech/user_wallet_service/constant/Role.java
@@ -2,7 +2,18 @@ package com.fintech.user_wallet_service.constant;
 
 
 public enum Role {
-    SUPER_ADMIN,
-    ADMIN,
-    USER
+    SUPER_ADMIN("Super Admin"),
+    ADMIN("Admin"),
+    USER("User");
+
+    private final String displayName;
+
+    // Constructor to associate presentable string with enum
+    Role(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
