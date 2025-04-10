b9f2ac1be9f13966c4b9029203d8eae9426cc39e
Gourav Singh <gourav.oct@gmail.com>
2025-04-11 00:43:13 +0530

    modified TransactionService

--- transaction-service/src/main/java/com/fintech/transaction_service/service/TransactionService.java
+++ transaction-service/src/main/java/com/fintech/transaction_service/service/TransactionService.java
@@ -24,57 +24,69 @@ public class TransactionService {
     @Value("${user.service.url}")  // Inject User Service Base URL from properties
     private String userServiceUrl;
 
-    private final RestTemplate restTemplate;
+//    private final RestTemplate restTemplate;
 
     private final TransactionRepository transactionRepository;
 
-    public TransactionService(TransactionRepository transactionRepository, RestTemplate restTemplate) {
+    private final TransactionProducer transactionProducer;
+
+//    public TransactionService(TransactionRepository transactionRepository, RestTemplate restTemplate) {
+//        this.transactionRepository = transactionRepository;
+//        this.restTemplate = restTemplate;
+//    }
+    public TransactionService(TransactionRepository transactionRepository, TransactionProducer transactionProducer) {
         this.transactionRepository = transactionRepository;
-        this.restTemplate = restTemplate;
+        this.transactionProducer = transactionProducer;
     }
 
 
+//    @Transactional
+//    public Transaction createTransaction(TransactionRequestDTO transactionRequest) {
+//        String byWalletId = transactionRequest.getByWalletId();
+//        String toWalletId = transactionRequest.getToWalletId();
+//        BigDecimal amount = transactionRequest.getAmount();
+//
+//        // Fetch Sender Wallet (Ensure it exists)
+//        ResponseEntity<WalletResponseDTO> senderWalletResponse = restTemplate.getForEntity(
+//                userServiceUrl + "/api/wallet/" + byWalletId, WalletResponseDTO.class);
+//
+//        if (!senderWalletResponse.hasBody()) {
+//            throw new RuntimeException("Sender wallet not found");
+//        }
+//
+//        // Fetch Receiver Wallet
+//        ResponseEntity<WalletResponseDTO> receiverWalletResponse = restTemplate.getForEntity(
+//                userServiceUrl + "/api/wallet/" + toWalletId, WalletResponseDTO.class);
+//
+//        if (!receiverWalletResponse.hasBody()) {
+//            throw new RuntimeException("Receiver wallet not found");
+//        }
+//
+//        // Debit Sender Wallet
+//        restTemplate.put(userServiceUrl + "/api/wallet/debit?userID="
+//                + senderWalletResponse.getBody().getUserId()
+//                + "&amount=" + amount, null);
+//
+//        // Credit Receiver Wallet
+//        restTemplate.put(userServiceUrl + "/api/wallets/credit?walletId="
+//                + toWalletId + "&amount="
+//                + amount, null);
+//
+//        // Create Transaction Log
+//        Transaction transaction = new Transaction(transactionRequest.getByUserId(), transactionRequest.getToUserId(),
+//                byWalletId, toWalletId, amount, TransactionType.DEBIT,
+//                transactionRequest.getMainExpenseType(), transactionRequest.getSubExpenseType(),
+//                Instant.now(), transactionRequest.getNote());
+//
+//        transactionRepository.save(transaction);
+//
+//        return transaction;
+//    }
+
     @Transactional
     public Transaction createTransaction(TransactionRequestDTO transactionRequest) {
-        String byWalletId = transactionRequest.getByWalletId();
-        String toWalletId = transactionRequest.getToWalletId();
-        BigDecimal amount = transactionRequest.getAmount();
-
-        // Fetch Sender Wallet (Ensure it exists)
-        ResponseEntity<WalletResponseDTO> senderWalletResponse = restTemplate.getForEntity(
-                userServiceUrl + "/api/wallet/" + byWalletId, WalletResponseDTO.class);
-
-        if (!senderWalletResponse.hasBody()) {
-            throw new RuntimeException("Sender wallet not found");
-        }
-
-        // Fetch Receiver Wallet
-        ResponseEntity<WalletResponseDTO> receiverWalletResponse = restTemplate.getForEntity(
-                userServiceUrl + "/api/wallet/" + toWalletId, WalletResponseDTO.class);
-
-        if (!receiverWalletResponse.hasBody()) {
-            throw new RuntimeException("Receiver wallet not found");
-        }
-
-        // Debit Sender Wallet
-        restTemplate.put(userServiceUrl + "/api/wallet/debit?userID="
-                + senderWalletResponse.getBody().getUserId()
-                + "&amount=" + amount, null);
-
-        // Credit Receiver Wallet
-        restTemplate.put(userServiceUrl + "/api/wallets/credit?walletId="
-                + toWalletId + "&amount="
-                + amount, null);
-
-        // Create Transaction Log
-        Transaction transaction = new Transaction(transactionRequest.getByUserId(), transactionRequest.getToUserId(),
-                byWalletId, toWalletId, amount, TransactionType.DEBIT,
-                transactionRequest.getMainExpenseType(), transactionRequest.getSubExpenseType(),
-                Instant.now(), transactionRequest.getNote());
-
-        transactionRepository.save(transaction);
-
-        return transaction;
+        transactionProducer.sendTransaction(transactionRequest);
+        return null; // optional: return dummy or async ID
     }
 
     public Transaction getTransactionById(String id) {
